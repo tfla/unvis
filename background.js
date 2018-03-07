c738c26f10688f006924964080101f2c9ece59b0
Timmy Larsson <timmy.larsson@bodhilinux.com>
2018-03-07 18:37:57 +0100

    Add addon code.

---
+++ background.js
@@ -0,0 +1,16 @@
+browser.contextMenus.create({
+  id: "unvis",
+  type: "normal",
+  title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
+  //title: browser.i18n.getMessage("openLinkWithUnvis"),
+  contexts: ["link"]
+} );
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  switch (info.menuItemId) {
+    case "unvis":
+      console.log("Opening link unv.is/" + info.linkUrl);
+      browser.tabs.create({url:"https://unv.is/" + info.linkUrl})
+      break;
+  }
+});
